f743ad3add1bd2e6673414ad0ef96258962cd331
shiren1118 <shiren1118@126.com>
2013-08-02 04:16:59 +0800

    add ghunit test

---
+++ no320.framework.test/test/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  test
+//
+//  Created by sang alfred on 8/1/13.
+//  Copyright (c) 2013 sang alfred. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <GHUnitIOS/GHUnit.h>
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, @"GHUnitIOSAppDelegate");
+    }
+}
